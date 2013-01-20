55434b357c7c11d06ff917e1baa567ed55be7e93
Grauniad <lhumphreys67@gmail.com>
2013-01-20 19:14:15 +0000

    Remove commented out code

--- Libraries/libUtils/dataReader.cpp
+++ Libraries/libUtils/dataReader.cpp
@@ -6,40 +6,6 @@ DataReader::DataReader(void *data, long l)
     rawData = reinterpret_cast<const unsigned char *>(data);
 }
 
-//
-//void DataReader::Read(long offset, void*dest, long size)  const
-//{
-//    if ( offset + size >= this->length ) {
-//        throw "Invalid read: source is not long enough";
-//    } else if ( offset < 0)  {
-//        throw "Invalid read from object: -'ve offset";
-//    }else {
-//        memcpy(dest,rawData+offset,size);
-//    }
-//}
-//
-//void DataReader::ReadString(long offset, std::string&dest)  const
-//{
-//    if ( offset >= this->length ) {
-//        throw "Invalid read: source is not long enough";
-//    } else if ( offset < 0)  {
-//        throw "Invalid read from object: -'ve offset";
-//    }else {
-//        dest = reinterpret_cast<const char *>(rawData+offset);             
-//    }
-//}
-//
-//unsigned char DataReader::Get(long offset)  const
-//{
-//    if ( offset >= this->length ) {
-//        throw "Invalid read: source is not long enough";
-//    } else if ( offset < 0)  {
-//        throw "Invalid read from object: -'ve offset";
-//    }else {
-//        return  rawData[offset];
-//    }
-//}
-//
 long DataReader::Next ( long offset, unsigned char c) const {
     long i;
     for (i = offset; i < len; ++i) {
