29037c8a3c7405ba90a0170b132337bd7ce18fd8
Luke Humphreys <lhumphreys@lhumphreys-laptop.(none)>
2013-03-17 08:15:50 +0000

    Activate teh elfStringTable Tests
    Identifies a bug with the section string table

--- Tests/elf2elf/elfStringTable.cpp
+++ Tests/elf2elf/elfStringTable.cpp
@@ -8,10 +8,10 @@
 #include <elf.h>
 #include "dataLump.h"
 
-ElfParser *p;
 DataLump<5000> outfile;
-ElfHeaderX86_64 header;
 SectionHeader* stringheaders;
+Elf64_Shdr* sections;
+Elf64_Shdr* stringTableHeader;
 
 int ValidHeader(stringstream& log );
 
@@ -22,10 +22,10 @@ int main(int argc, const char *argv[])
      */
     ElfFileReader f("../elf2elf/isYes/isYes.o");
     
-    p = new ElfParser(f);
+    ElfParser p(f);
     
     //parse the input file
-    ElfContent content = p->Content();
+    ElfContent content = p.Content();
     ElfFile file( content);
     
     // Write out the data
@@ -34,28 +34,37 @@ int main(int argc, const char *argv[])
     // read in the section data
     BinaryReader readPos(outfile);
     ElfHeaderX86_64 header(readPos);
-    readPos += header.SectionTableStart() 
-             + header.StringTableIndex() * header.SectionHeaderSize();
+    sections = new Elf64_Shdr[header.Sections()];
+
+    readPos = header.SectionTableStart();
+    for ( int i=0; i< header.Sections(); i++ ) {
+	readPos >> sections[i];
+    }
+
+    stringTableHeader = &(sections[header.StringTableIndex()]);
+
     Test("Header format",(loggedTest)ValidHeader).RunTest();
-    delete p;
 
     return 0;
 }
 
 int ValidHeader(stringstream& log ) {
     int retCode = 0;
-    stringheader = new SectionHeader(readPos);
 
-    if ( stringheader->sh_type != SHT_STRTAB ) {
-        log << stringheader->sh_type << " , " << SHT_STRTAB << endl;
+    if ( stringTableHeader->sh_type != SHT_STRTAB ) {
+        log << stringTableHeader->sh_type << " , " << SHT_STRTAB << endl;
         log << "Invalid section type" << endl;
+	log << "Header Block: " << endl;
+	log << "Flags: " << stringTableHeader->sh_flags << endl;
+	log << "Type: " << stringTableHeader->sh_type << endl;
+	log << "AddrAlign: " << stringTableHeader->sh_addralign << endl;
+	log << "Offset: " << stringTableHeader->sh_offset << endl;
+	log << "Size: " << stringTableHeader->sh_size << endl;
+	log << "Entry Size: " << stringTableHeader->sh_entsize << endl;
+	log << "Name Addr: " << stringTableHeader->sh_name << endl;
+	log << "Addr: " << stringTableHeader->sh_addr << endl;
         retCode = 1;
     }
-
-    if ( retCode == 0 ) {
-    }
-
-    delete stringheader;
 
     return retCode;
 }
