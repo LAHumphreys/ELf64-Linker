211610dadc9cf0b678335115f9751c4cfec90056
Luke Humphreys <lhumphreys67@gmail.com>
2013-03-17 12:41:03 +0000

    Make the DEFER command available to test scripts
     - Added the DEFER submodule
     - Added to the include path, but ONLY for test scripts - it should not
       be used for program code

--- Tests/elf2elf/elfStringTable.cpp
+++ Tests/elf2elf/elfStringTable.cpp
@@ -7,6 +7,7 @@
 #include "tester.h"
 #include <elf.h>
 #include "dataLump.h"
+#include "defer.h"
 
 DataLump<5000> outfile;
 SectionHeader* stringheaders;
@@ -36,6 +37,8 @@ int main(int argc, const char *argv[])
     BinaryReader readPos(outfile);
     ElfHeaderX86_64 header(readPos);
     sections = new Elf64_Shdr[header.Sections()];
+    DEFER (
+    )
 
     readPos = header.SectionTableStart();
     for ( int i=0; i< header.Sections(); i++ ) {
@@ -45,6 +48,8 @@ int main(int argc, const char *argv[])
     stringTableHeader = &(sections[header.StringTableIndex()]);
 
     Test("Header format",(loggedTest)ValidHeader).RunTest();
+    //Test("Header format",(loggedTest)SectionNames).RunTest();
+
 
     return 0;
 }
@@ -55,15 +60,15 @@ int ValidHeader(stringstream& log ) {
     if ( stringTableHeader->sh_type != SHT_STRTAB ) {
         log << stringTableHeader->sh_type << " , " << SHT_STRTAB << endl;
         log << "Invalid section type" << endl;
-	log << "Header Block: " << endl;
-	log << "Flags: " << stringTableHeader->sh_flags << endl;
-	log << "Type: " << stringTableHeader->sh_type << endl;
-	log << "AddrAlign: " << stringTableHeader->sh_addralign << endl;
-	log << "Offset: " << stringTableHeader->sh_offset << endl;
-	log << "Size: " << stringTableHeader->sh_size << endl;
-	log << "Entry Size: " << stringTableHeader->sh_entsize << endl;
-	log << "Name Addr: " << stringTableHeader->sh_name << endl;
-	log << "Addr: " << stringTableHeader->sh_addr << endl;
+        log << "Header Block: " << endl;
+        log << "Flags: " << stringTableHeader->sh_flags << endl;
+        log << "Type: " << stringTableHeader->sh_type << endl;
+        log << "AddrAlign: " << stringTableHeader->sh_addralign << endl;
+        log << "Offset: " << stringTableHeader->sh_offset << endl;
+        log << "Size: " << stringTableHeader->sh_size << endl;
+        log << "Entry Size: " << stringTableHeader->sh_entsize << endl;
+        log << "Name Addr: " << stringTableHeader->sh_name << endl;
+        log << "Addr: " << stringTableHeader->sh_addr << endl;
         retCode = 1;
     }
 
