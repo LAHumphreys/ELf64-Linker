f68be839d4fa12616aab8ffb6e3e16d6b1ccb871
Luke Humphreys <lhumphreys67@gmail.com>
2013-05-13 08:38:48 +0100

    Removing some tabs

--- Libraries/libTest/tester.cpp
+++ Libraries/libTest/tester.cpp
@@ -10,29 +10,29 @@ using namespace std;
 
 Test::Test(string description, std::function<int(void)> test) {
     this->test = test;
-	this->testType = BASIC;
+    this->testType = BASIC;
     this->description = description;
 }
 
 Test::Test(string description, std::function<int(testLogger& log)> test) {
-	this->testLogged = test;
-	this->testType = LOGGED;
+    this->testLogged = test;
+    this->testType = LOGGED;
     this->description = description;
 }
 
 void Test::RunTest() {
     cout << description << ": ";
-	testLogger log;
+    testLogger log;
     #ifdef __PROFILE__TESTS
         auto start = chrono::high_resolution_clock::now();
     #endif
 
-	int result;
-	if ( testType == BASIC ) {
-	    result = test();
-	} else {
-		result = testLogged(log);
-	}
+    int result;
+    if ( testType == BASIC ) {
+        result = test();
+    } else {
+        result = testLogged(log);
+    }
     #ifdef __PROFILE__TESTS
         auto stop = chrono::high_resolution_clock::now();
         if (result == 0 ) {
@@ -41,7 +41,7 @@ void Test::RunTest() {
             if ( duration == 0 ) {
                 duration = 1.0 / TICKS_PER_SECOND;
             }
-            long target = 60 * TICKS_PER_SECOND; 
+            long target = 15 * TICKS_PER_SECOND; 
             long dur    = duration * TICKS_PER_SECOND +1;
             cout << duration << " , "  << target << " , " << dur << endl;
             for ( long t = dur; t < target; t+=dur )
@@ -55,10 +55,10 @@ void Test::RunTest() {
         cout << "TEST PASSED" << endl;
     } else {
         cout << "TEST FAILED" << endl;
-		if ( testType == LOGGED ) {
-			cout << "Log follows: " << endl;
-			cout << log.str() << endl;
-		}
+        if ( testType == LOGGED ) {
+            cout << "Log follows: " << endl;
+            cout << log.str() << endl;
+        }
         exit(result);
     }
 }
